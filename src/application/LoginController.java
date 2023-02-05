27c098f687438538598654edd61b29f9d88ea431
uamiatoii <pierrejaphetkonan@gmail.com>
2023-02-05 22:26:00 +0000

    Ajout de la vue produit

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,10 +1,16 @@
 package application;
 
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
@@ -23,9 +29,19 @@ public class LoginController {
     
     @FXML
     private Button cancelButton;
+    
+    @FXML
+    private Button loginButton;
 
-    public void boutonConnexion(ActionEvent e) {
+    public void boutonConnexion(ActionEvent e) throws IOException {
+    	
 //    	appel de la vue du menu
+    	Parent pageParent = FXMLLoader.load(getClass().getResource("vueProduits.fxml"));
+		Scene pageScene = new Scene(pageParent);
+		Stage stage = (Stage) loginButton.getScene().getWindow();
+		stage.setScene(pageScene);
+		stage.show();
+    	
     }
     public void boutonAnnule(ActionEvent e) {
     	Stage stage = (Stage) cancelButton.getScene().getWindow();
